ca4f3da0be57570895345d7c36b5607af25a546f
毛燕丰 <kfg4015@dingtalk.com>
2024-07-25 17:23:03 +0800

    init

---
+++ helloworld.py
@@ -0,0 +1,22 @@
+#my firtst code
+print("hello world")
+
+'''多行注释,do nothing
+'''
+name='myf'
+print(name)
+
+# 字符串切片 str[startIndex:endIndex],endIndex不包含
+firstName=name[0:1]
+print(firstName)
+
+word="\ngood good study,\nday day up!"
+print(word)
+#在字符串前加r，可以输出原始字符串，不进行转义
+rword=r"\ngood good study,\nday day up!"
+print(rword)
+
+if True :
+    print("good")
+else :
+    print("bad")
---
+++ testStr.py
@@ -0,0 +1,11 @@
+print("\a")
+str="abc"
+print(str * 2)
+
+
+print("my name is %s"%(str))
+
+print(f"your name is {str}")
+
+
+print("aa".join(","))
