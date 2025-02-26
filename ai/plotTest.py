1306b4a9fcb9365ab0560a5cd485fe6491b57a6c
毛燕丰 <kfg4015@dingtalk.com>
2025-02-26 17:58:13 +0800

    线性回归模型推导笔记

---
+++ ai/plotTest.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+if __name__ == '__main__':
+    np.random.randint
+    # list1 = [x for x in range(10)]
+    # value1 = [np.power(x, 3) for x in list1]
+    # plt.plot(list1, value1)
+    # #value2 = [np.power(x, 4) for x in list1]
+    # #plt.plot(list1, value2)
+    #
+    # for i in range(len(list1)):
+    #     plt.text(list1[i], value1[i], f'({list1[i]},{value1[i]})', fontsize=12, ha='right', va='bottom')
+    # plt.show()
---
+++ ai/runCode/LinearTest.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# @Time : 2025/2/24 11:44
+# @Author : myf
+# @Software : PyCharm
+
